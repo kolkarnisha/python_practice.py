966280ad99268f60eff94dfa35fdba79c7ed587c
Kolkar Nisha <neeshakolkar@gmail.com>
2026-09-04 18:48:44 +0530

    Implement substring search, reverse, and palindrome functions
    Added multiple functions for substring search, string reversal, and palindrome check.

--- revise.str.py
+++ revise.str.py
@@ -63,6 +63,60 @@ def seil(str,sub):
       print(f"substring found at start index {i} to - end {end}")
     # print("substring not found")
 seil("nishataj","taj")
+
+
+
+''' finding out indexes of substring
+find start to end index'''
+def sei(str,sub):
+  india=str.find(sub)
+  if india==-1:
+    print("substring not found")
+  print(f"substring found at index {india} to- {india+len(sub)-1}")
+sei("nishataj","taj")
+''' finding out substring in loop'''
+def seil(str,sub):
+  m=len(str)
+  n=len(sub)
+  f=len(sub)-1
+  for i in range(m-n+1):
+    if str[i:i+n]==sub:
+      end=i+f
+      print(f"substring found at start index {i} to - end {end}")
+    # print("substring not found")
+seil("nishataj","taj")
+'''finding out substring in a dictionary'''
+def dic(str,sub):
+  empty={}
+  m=len(str)
+  n=len(sub)
+  for i in range(m-n+1):
+    if str[i:i+n]==sub:
+      for j in range(n):
+        empty[sub[j]]=i+j
+    print(empty)
+dic("nishataj","taj")
+'''reverse of string'''
+def rev(str):
+  print("".join(reversed(str)))
+rev("nisha")
+
+def rev(str):
+  empty=""
+  for i in str:
+    empty=i+empty
+  print(empty)
+rev("nisha")
+def palindrone(text):
+  text.lower().replace('','')
+  text=text[::-1]
+  print(text)
+s=input()
+if palindrone(s):
+  print("yes")
+# else:
+#   print("no")
+  
       
 
   
