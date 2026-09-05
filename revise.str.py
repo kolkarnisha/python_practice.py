717a7a4d1ffa6f92cdf816c712cc1da88e7e3143
Kolkar Nisha <neeshakolkar@gmail.com>
2026-09-05 18:24:19 +0530

    Implement string manipulation functions
    Added multiple functions to find characters, substrings, and their indices in a string, along with string reversal and palindrome check.

--- revise.str.py
+++ revise.str.py
@@ -116,6 +116,100 @@ if palindrone(s):
   print("yes")
 # else:
 #   print("no")
+
+
+
+
+print("date 05-09-2026")
+'''finding out a target'''
+def find():
+  str="nisha"
+  target="a"
+  for i in str:
+    if i==target:
+      print(i)
+find()
+'''finding out index'''
+def index(str,target):
+  for i,j in enumerate(str):
+    if j==target:
+      print(i)
+index("nisha","a")
+'''find all indexes'''
+def all(str,target):
+  empty=[]
+  for i,j in enumerate(str):
+    if j==target:
+      empty.append(i)
+  print(empty)
+all("nishajareentaj","a")
+'''finding out a substring'''
+def substr(str,sub):
+  m=len(str)
+  n=len(sub)
+  for i in range(m-n+1):
+    if str[i:i+n]==sub:
+      print(i)
+substr("nishataj","taj")
+'''finding out substring start to end'''
+def se(str,sub):
+  india=str.find(sub)
+  if india==-1:
+    print("substring not found")
+  print(f"substring found at index{india}-{india+len(sub)-1}")
+se("nishataj","taj")
+'''finding out substring in dictionary'''
+def dic(str,sub):
+  empty={}
+  m=len(str)
+  n=len(sub)
+  # f=len(sub)-1
+  for i in range(m-n+1):
+    if str[i:i+n]==sub:
+      for j in range(n):
+        empty[sub[j]]=i+j
+  print(empty)
+dic("nishataj","taj")
+'''def finding out indexes in loop'''
+def loop(str,sub):
+    empty=[]
+    n=len(sub)
+    m=len(str)
+    f=len(sub)-1
+    for i in range(m-n+1):
+      if str[i:i+n]==sub:
+        end=i+f
+        print(f"substring found at index {i} - {end}")
+    # print("not ")
+loop("nishataj","taj")
+''' reversing a string using inbuilt method'''
+def rev(str):
+  print("".join(reversed(str)))
+  # print(str)
+rev("nisha")
+print("".join(reversed("taj")))
+'''reversing a string'''
+def reverse(str):
+  empty=''
+  for i in str:
+     empty=i+empty
+  print(empty)
+reverse("nisha")
+'''palindrone'''
+def palin(str):
+  str.lower().replace("","")
+  print(str[::-1])
+str=input()
+if palin(str):
+  print("yes palindrone")
+# else:
+#   print("no")
+
+
+
+
+
+        
   
       
 
